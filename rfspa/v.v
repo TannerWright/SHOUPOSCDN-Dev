51379a3f1ca9d37a4538d313d7753f2368364690
Tanner <tanner@shoutz.com>
2015-10-27 17:36:40 -0500

    10-27-2015 #2

--- rfspa/v.v
+++ rfspa/v.v
@@ -1,11 +1,11 @@
-{0.0.0.1} CONTINUE
-NORMAL[bin\rfspa.exe]=0.0.0.0;
-NORMAL[bin\rfspa.exe.config]=0.0.0.0;
-NORMAL[lib\dtsvc.dll]=0.0.0.0;
-NORMAL[lib\Newtonsoft.Json.dll]=0.0.0.0;
-NORMAL[lib\Newtonsoft.Json.xml]=0.0.0.0;
-NORMAL[lib\objlib.dll]=0.0.0.0;
-DELETE[lib\sndlib.dll]=;
-NORMAL[lib\System.Net.Http.Formatting.dll]=0.0.0.0;
-NORMAL[lib\System.Net.Http.Formatting.xml]=0.0.0.0;
-NORMAL[lib\Test_Configurations.dll]=0.0.0.0;
+{0.0.0.1} CONTINUE;
+NORMAL[bin\rfspa.exe]0.0.0.0;
+NORMAL[bin\rfspa.exe.config]0.0.0.0;
+NORMAL[lib\dtsvc.dll]0.0.0.0;
+NORMAL[lib\Newtonsoft.Json.dll]0.0.0.0;
+NORMAL[lib\Newtonsoft.Json.xml]0.0.0.0;
+NORMAL[lib\objlib.dll]0.0.0.0;
+DELETE[lib\sndlib.dll];
+NORMAL[lib\System.Net.Http.Formatting.dll]0.0.0.0;
+NORMAL[lib\System.Net.Http.Formatting.xml]0.0.0.0;
+NORMAL[lib\Test_Configurations.dll]0.0.0.0;
