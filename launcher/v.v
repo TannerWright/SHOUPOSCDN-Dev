d0694b16fd0405b52982e0ad2e2d2fd9d5ed1c5d
Tanner <tanner@shoutz.com>
2015-10-28 16:27:21 -0500

    0.1.2,3 Update

--- launcher/v.v
+++ launcher/v.v
@@ -1,2 +1,2 @@
-{0.1.3.355} CONTINUE;
-NORMAL[0,bin/launcher.exe,app.exe]0.1.3.355;
+{0.1.4.426} CONTINUE;
+NORMAL[0,bin/launcher.exe,app.exe]0.1.4.426;
--- prelauncher/v.v
+++ prelauncher/v.v
@@ -1,2 +1,2 @@
-{0.1.2.355} CONTINUE;
-NORMAL[0,bin/prelauncher.exe,app.exe]0.1.2.355;
+{0.1.3.426} CONTINUE;
+NORMAL[0,bin/prelauncher.exe,app.exe]0.1.3.426;
