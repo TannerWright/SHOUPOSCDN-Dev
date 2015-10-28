3a11d4df91417c15768268402c451d4c3b733f2d
TannerWright <tanner@shoutz.com>
2015-10-28 14:20:04 -0500

    Update v.v

--- rfspa/v.v
+++ rfspa/v.v
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 {0.6.0.156} CONTINUE;
 NORMAL[0,bin/rfspa.exe,app.exe]0.6.0.156;
 NORMAL[1,bin/rfspa.exe.config,app.exe.config]0.6.0.156;
@@ -16,16 +15,3 @@ NORMAL[13,lib/Test_Configurations.pdb,lib/Test_Configurations.pdb]0.2.3.1008;
 NORMAL[14,lib/dtsvc.pdb,lib/dtsvc.pdb]0.2.3.443;
 NORMAL[15,bin/rfspa.pdb,app.pdb]0.0.0.0;
 NORMAL[16,lib/objlib.pdb,lib/objlib.pdb]0.2.3.1008;
-=======
-{0.0.0.1} CONTINUE;
-NORMAL[0,bin/rfspa.exe,app.exe]0.0.0.0;
-NORMAL[1,bin/rfspa.exe.config,app.exe.config]0.0.0.0;
-NORMAL[2,lib/dtsvc.dll,lib/dtsvc.dll]0.0.0.0;
-NORMAL[3,lib/Newtonsoft.Json.dll,lib/Newtonsoft.Json.dll]0.0.0.0;
-NORMAL[4,lib/Newtonsoft.Json.xml,lib/Newtonsoft.Json.xml]0.0.0.0;
-NORMAL[5,lib/objlib.dll,lib/objlib.dll]0.0.0.0;
-DELETE[6,lib/sndlib.dll,lib/sndlib.dll];
-NORMAL[7,lib/System.Net.Http.Formatting.dll,lib/System.Net.Http.Formatting.dll]0.0.0.0;
-NORMAL[8,lib/System.Net.Http.Formatting.xml,lib/System.Net.Http.Formatting.xml]0.0.0.0;
-NORMAL[9,lib/Test_Configurations.dll,lib/Test_Configurations.dll]0.0.0.0;
->>>>>>> origin/master
