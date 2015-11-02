d47901f3c39e2584d09d50bd82e4e842abbd4540
TannerWright <tanner@shoutz.com>
2015-11-02 14:22:34 -0600

    Update v.v

--- rfspa/v.v
+++ rfspa/v.v
@@ -1,10 +1,10 @@
-{0.6.2.429} CONTINUE;
-NORMAL[0,bin/rfspa.exe,app.exe]0.6.2.429;
+{0.6.5.429} CONTINUE;
+NORMAL[0,bin/rfspa.exe,app.exe]0.6.5.219;
 NORMAL[1,bin/rfspa.exe.config,app.exe.config]0.6.0.156;
-NORMAL[2,lib/dtsvc.dll,lib/dtsvc.dll]0.2.3.443;
+NORMAL[2,lib/dtsvc.dll,lib/dtsvc.dll]0.3.0.219;
 NORMAL[3,lib/Newtonsoft.Json.dll,lib/Newtonsoft.Json.dll]7.0.1.18622;
 NORMAL[4,lib/Newtonsoft.Json.xml,lib/Newtonsoft.Json.xml]7.0.1.18622;
-NORMAL[5,lib/objlib.dll,lib/objlib.dll]0.2.3.1008;
+NORMAL[5,lib/objlib.dll,lib/objlib.dll]0.3.0.219;
 DELETE[6,lib/sndlib.dll,lib/sndlib.dll];
 NORMAL[7,lib/System.Net.Http.Formatting.dll,lib/System.Net.Http.Formatting.dll]4.0.20710.0;
 NORMAL[8,lib/System.Net.Http.Formatting.xml,lib/System.Net.Http.Formatting.xml]0.0.0.0;
@@ -12,6 +12,6 @@ NORMAL[10,lib/Microsoft.PointOfService.dll,lib/Microsoft.PointOfService.dll]1.14
 NORMAL[11,lib/Microsoft.PointOfService.xml,lib/Microsoft.PointOfService.xml]1.14.2.0;
 NORMAL[12,lib/Test_Configurations.dll,lib/Test_Configurations.dll]0.2.3.1008;
 NORMAL[13,lib/Test_Configurations.pdb,lib/Test_Configurations.pdb]0.2.3.1008;
-NORMAL[14,lib/dtsvc.pdb,lib/dtsvc.pdb]0.2.3.443;
-NORMAL[15,bin/rfspa.pdb,app.pdb]0.6.2.429;
-NORMAL[16,lib/objlib.pdb,lib/objlib.pdb]0.2.3.1008;
+NORMAL[14,lib/dtsvc.pdb,lib/dtsvc.pdb]0.3.0.219;
+NORMAL[15,bin/rfspa.pdb,app.pdb]0.6.5.219;
+NORMAL[16,lib/objlib.pdb,lib/objlib.pdb]0.3.0.219;
